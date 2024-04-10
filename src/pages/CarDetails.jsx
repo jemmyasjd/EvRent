b8bb026a66b4fdd70aec6df4e6ebfad28fda5ac0
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2024-04-10 10:12:47 +0530

    Required changes in CarDetails.jsx

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -50,7 +50,7 @@ const CarDetails = () => {
 
                 <div className="d-flex align-items-center gap-5 mb-4 mt-3">
                   <h6 className="rent__price fw-bold fs-4">
-                    ${singleCarItem.price}.00 / Day
+                    Rs{singleCarItem.price}.00 / Hour
                   </h6>
 
                   <span className="d-flex align-items-center gap-2">
