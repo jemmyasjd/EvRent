52dd155e38e8da643f8e81ca0ec8f4614e6909d5
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2024-04-10 11:04:56 +0530

    Required changes in AboutSection.jsx

--- src/components/UI/AboutSection.jsx
+++ src/components/UI/AboutSection.jsx
@@ -20,34 +20,32 @@ const AboutSection = ({ aboutClass }) => {
               <h4 className="section__subtitle">About Us</h4>
               <h2 className="section__title">Welcome to car rent service</h2>
               <p className="section__description">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Voluptatum blanditiis esse accusantium dignissimos labore
-                laborum. Veniam, corporis mollitia temporibus, in quaerat vero
-                deleniti amet dolorem repudiandae, pariatur nam dolore! Impedit
-                neque sit ad temporibus quam similique dolor ipsam praesentium
-                sunt.
+                Our mission is to promote sustainable mobility by providing convenient and affordable electric vehicle rental services. 
+                We aim to reduce carbon emissions and contribute to a cleaner environment. 
+                At EV Car Rent Service, we offer a diverse fleet of electric vehicles, ranging from compact cars to spacious SUVs. 
+                Our vehicles are equipped with cutting-edge technology and are well-maintained to ensure a smooth and enjoyable ride.
               </p>
 
               <div className="about__section-item d-flex align-items-center">
                 <p className="section__description d-flex align-items-center gap-2">
-                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  <i class="ri-checkbox-circle-line"></i> Eco-friendly 
                   amet.
                 </p>
 
                 <p className="section__description d-flex align-items-center gap-2">
-                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  <i class="ri-checkbox-circle-line"></i> Customer-centric 
                   amet.
                 </p>
               </div>
 
               <div className="about__section-item d-flex align-items-center">
                 <p className="section__description d-flex align-items-center gap-2">
-                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  <i class="ri-checkbox-circle-line"></i> Convenience 
                   amet.
                 </p>
 
                 <p className="section__description d-flex align-items-center gap-2">
-                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  <i class="ri-checkbox-circle-line"></i> Reliable 
                   amet.
                 </p>
               </div>
