96f98a7dee55e103ac796930511ceaea69fe5c42
jemmyasjd <jemmy33.jd@gmail.com>
2024-04-24 00:24:37 +0530

    updated images

--- src/assets/data/carData.jsx
+++ src/assets/data/carData.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 // import all images from assets/images directory
-import img01 from "../all-images/cars-img/nissan-offer.png";
-import img02 from "../all-images/cars-img/offer-toyota.png";
-import img03 from "../all-images/cars-img/bmw-offer.png";
-import img04 from "../all-images/cars-img/nissan-offer.png";
-import img05 from "../all-images/cars-img/offer-toyota.png";
-import img06 from "../all-images/cars-img/mercedes-offer.png";
-import img07 from "../all-images/cars-img/toyota-offer-2.png";
-import img08 from "../all-images/cars-img/mercedes-offer.png";
+import img01 from "../all-images/cars-img/list_1.png";
+import img02 from "../all-images/cars-img/list_2.png";
+import img03 from "../all-images/cars-img/demo.png";
+import img04 from "../all-images/cars-img/list_3.png";
+import img05 from "../all-images/cars-img/list_6.png";
+import img06 from "../all-images/cars-img/list_5.png";
+import img07 from "../all-images/cars-img/list_6.png";
+import img08 from "../all-images/cars-img/list_1.png";
+
 import { RentContext } from "../../Context/RentContext";
 
 
@@ -23,12 +24,13 @@ const useCarData = () => {
 
 
 
+
 const carData = [
   {
     id: 1,
     brand: "Tesla",
     rating: 112,
-    carName: "Tesla Malibu",
+    carName: "BMW Malibu",
     imgUrl: img01,
     model: "Model 3",
     price: 50,
@@ -44,7 +46,7 @@ const carData = [
     id: 2,
     brand: "Toyota",
     rating: 102,
-    carName: "Toyota Aventador",
+    carName: "Tata Nexon",
     imgUrl: img02,
     model: "Model-2022",
     price: 50,
@@ -76,7 +78,7 @@ const carData = [
     id: 4,
     brand: "Nissan",
     rating: 102,
-    carName: "Nissan Mercielago",
+    carName: "Tata Punch",
     imgUrl: img04,
     model: "Model-2022",
     price: 70,
@@ -92,7 +94,7 @@ const carData = [
     id: 5,
     brand: "Ferrari",
     rating: 94,
-    carName: "Ferrari Camry",
+    carName: "MG Camry",
     imgUrl: img05,
     model: "Model-2022",
     price: 45,
@@ -108,7 +110,7 @@ const carData = [
     id: 6,
     brand: "Mercedes",
     rating: 119,
-    carName: "Mercedes Benz XC90",
+    carName: "Hyundai XC90",
     imgUrl: img06,
     model: "Model-2022",
     price: 85,
--- src/assets/data/serviceData.js
+++ src/assets/data/serviceData.js
@@ -3,42 +3,42 @@ const serviceData = [
     id: 1,
     title: "City Transfer",
     icon: "ri-map-pin-2-line",
-    desc: "Explore the city in style and sustainability with EV Rent Car Service. Our City Transfer service provides seamless transportation within urban areas, offering comfortable rides in our fleet of electric vehicles.",
+    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
   },
 
   {
     id: 2,
     title: "Whole  City Tour",
     icon: "ri-community-line",
-    desc: "Embark on an unforgettable journey through the city with EV Rent Car Service. Our Whole City Tour service offers a comprehensive exploration of local attractions and landmarks, all while enjoying the comfort and sustainability of our electric vehicles.",
+    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
   },
 
   {
     id: 3,
     title: "Unlimited Miles Car Rental",
     icon: "ri-roadster-line",
-    desc: "Experience freedom without limits with EV Rent Car Service's Unlimited Miles Car Rental. Whether it's a spontaneous road trip or a long-distance journey, enjoy unlimited mileage with our electric vehicles and explore new horizons with ease.",
+    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
   },
 
   {
     id: 4,
     title: "Fast & Easy Booking",
     icon: "ri-timer-flash-line",
-    desc: "Streamline your travel plans with EV Rent Car Service's Fast & Easy Booking. Our user-friendly platform allows you to book your electric vehicle quickly and effortlessly, ensuring a seamless rental experience from start to finish.",
+    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
   },
 
   {
     id: 5,
     title: "Many Pickup Locations",
     icon: "ri-map-pin-line",
-    desc: "Convenience meets flexibility with EV Rent Car Service's Many Pickup Locations. With multiple pickup points conveniently located throughout the area, you can easily access our electric vehicles and embark on your journey with ease.",
+    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
   },
 
   {
     id: 6,
     title: "Airport Transfer",
     icon: "ri-flight-takeoff-line",
-    desc: "Arrive in style and comfort with EV Rent Car Service's Airport Transfer. Skip the hassle of traditional transportation and enjoy a smooth, stress-free journey to and from the airport in our eco-friendly electric vehicles.",
+    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
   },
 ];
 
