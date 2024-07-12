3f926c693dbcb6eb5dcf3261542bef75aec5d718
jemmyasjd <jemmy33.jd2gmail.com>
2024-07-12 17:31:55 +0530

    updated backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,24 @@
 const {connectDB} = require('./connection.jsx');
 const express = require('express');
 const cors = require('cors');
-const contactRouter = require('./routers/contact');
+require('dotenv').config();
+const contactRouter = require('./routers/contact.js');
 const carRoutes = require('./routers/car.js');
-const corsConfig = {
-    origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-}
+
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 
 connectDB('mongodb+srv://jemmy33jd:Mongodb.33@cluster0.1auwi0e.mongodb.net/car_db');
 
 
 
-app.use(cors(corsConfig));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+
 app.use('/contact/add', contactRouter)
 app.use('/cars', carRoutes);
 
